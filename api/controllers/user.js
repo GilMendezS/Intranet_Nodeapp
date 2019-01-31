fd39a318eab09b208c0dcd08addfc04471e9d36e
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2019-01-31 16:10:25 -0600

    :pencil: creating and listing user

---
+++ api/controllers/user.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const rootDir = require('../utils/path');
+const sequelize = require('../utils/database');
+
+const User = sequelize.import(path.join(rootDir,'models','user.js'))
+
+exports.getUsers = (req, res ,next) =>  {
+    User.findAll()
+    .then(users => {
+        console.log(users)
+        return res.status(200).json(
+            users
+        )
+    })
+    .catch(err => {
+        console.log(err)
+        return res.status(500).json({
+            message: 'Error fetching the users',
+            success: false
+        })
+    })
+}
+
+exports.addUser = (req, res ,next) => {
+    User.create({
+        name: req.body.name,
+        lastname: req.body.lastname,
+        email: req.body.email,
+        password: req.body.password,
+        employee_number: req.body.employee_number
+    })
+    .then(user => {
+        return res.status(200).json({
+            message: 'User created successfully!',
+            success: true,
+            user
+        })
+    })
+    .catch(err => {
+        console.log(err)
+        return res.status(500).json({
+            message: 'Something was wrong',
+            error: err
+        })
+    })
+}
--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const cors = require('cors');
 const express = require('express');
-const config = require('dotenv').config()
+const config = require('dotenv').config();
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
 
 app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
+//routes
+const usersRoutes = require('./api/routes/user');
+const API_VERSION = process.env.API_VERSION;
+
+app.use(`${API_VERSION}/users`, usersRoutes);
+
 app.listen(PORT, () => {
-    console.log(`Http server on :${PORT}`)
+    console.log(`Http server on :${PORT}`);
 })
 
