a415f7948ac5ebf7d7b48e4f6860e1486cee4759
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2019-01-31 16:09:29 -0600

    :bug: db connection modified

--- api/utils/database.js
+++ api/utils/database.js
@@ -3,16 +3,13 @@ const Sequelize = require('sequelize');
 const DB_HOST=process.env.DB_HOST;
 const DB_USER=process.env.DB_USER;
 const DB_PASSWORD=process.env.DB_PASSWORD;
-const DB_SCHEMA=process.env.DB_SHEMA;
+const DB_SCHEMA=process.env.DB_SCHEMA;
 const DB_DIALECT=process.env.DB_DIALECT;
 const DB_LOGS = process.env.DB_LOGS;
 
-
-const sequelize = new Sequelize(DB_SCHEMA, {
+const sequelize = new Sequelize(DB_SCHEMA,DB_USER, DB_PASSWORD,{
     host: DB_HOST,
-    username: DB_USER,
-    password: DB_PASSWORD,
     dialect: DB_DIALECT,
-    logging: DB_LOGS
+    logging: console.log
 })
 module.exports = sequelize;
