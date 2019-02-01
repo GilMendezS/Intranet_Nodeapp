ae32ed949f5d2a11910742e971bc0576db6ebdf6
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2019-02-01 11:58:51 -0600

    :+1: crud operatios for positions

--- api/controllers/position.js
+++ api/controllers/position.js
@@ -13,3 +13,67 @@ exports.getPositions = async (req, res, next) => {
         })
     }
 }
+exports.addPosition = async (req, res, next) => {
+    try {
+        const newPosition = await Position.create({
+            title: req.body.title,
+            DepartmentId: req.body.DepartmentId,
+            AreaId: req.body.AreaId
+        })
+        return res.status(200).json({
+            message: 'Position cretaed',
+            data: newPosition,
+            success: true
+        })
+    } catch (error) {
+        return res.status(200).json({
+            message: 'Error creating the position',
+            error
+        })
+    }
+}
+exports.getPosition = async (req, res, next) => {
+    try {
+        const positionId = req.params.id;
+        const position = await Position.findById(positionId, {include:['Department','Area']})
+        return res.status(200).json({
+            data: position
+        })
+    } catch (error) {
+        return res.status(200).json({
+            message: 'Error fetching the position',
+            error
+        })
+    }
+}
+exports.updatePosition = async (req, res, next) => {
+    try {
+        const positionId = req.params.id;
+        const updatedPosition = await Position.update(req.body, {where: {id: positionId}});
+        return res.status(200).json({
+            message: 'Position updated',
+            success: true,
+            data: updatedPosition
+        })
+    } catch (error) {
+        return res.status(200).json({
+            message: 'Error updating the position',
+            error
+        })
+    }
+}
+exports.removePosition = async (req, res, next) => {
+    try {
+        const positionId = req.params.id;
+        await Position.destroy({where:{id:positionId}});
+        return res.status(200).json({
+            message: 'Position removed',
+            success: true
+        })
+    } catch (error) {
+        return res.status(200).json({
+            message: 'Error removing the position',
+            error
+        })
+    }
+}
--- api/routes/position.js
+++ api/routes/position.js
@@ -4,4 +4,12 @@ const PositionController = require('../controllers/position');
 
 router.get('/', PositionController.getPositions);
 
+router.post('/', PositionController.addPosition);
+
+router.get('/:id', PositionController.getPosition);
+
+router.put('/:id', PositionController.updatePosition);
+
+router.delete('/:id', PositionController.removePosition);
+
 module.exports = router;
