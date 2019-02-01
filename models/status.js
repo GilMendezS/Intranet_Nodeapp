2e2e9f97bda4370ef0c323788af302106d3e8cc4
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2019-02-01 13:09:48 -0600

    :+1: scopes

--- models/status.js
+++ models/status.js
@@ -1,12 +1,29 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
+  const PROJECTS = 'projects';
+  const VIATICS = 'viatics';
   const Status = sequelize.define('Status', {
     name: DataTypes.STRING,
     title: DataTypes.STRING,
     for: DataTypes.STRING
-  }, {});
+  }, {
+    scopes: {
+      projects: {
+        where: {
+          for: PROJECTS
+        }
+      },
+      viatics: {
+        where: {
+          for: VIATICS
+        }
+      }
+    }
+  });
   Status.associate = function(models) {
     // associations can be defined here
   };
+  
+
   return Status;
 };
