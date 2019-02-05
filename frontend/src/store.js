33f13288a84dd6e91783ea83678be6d3b4cfd3c9
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2019-02-05 10:47:07 -0600

    :+1: main store with url for api and new module added for projects

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,16 +1,25 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import auth from './stores/auth';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  modules: {
+    auth
+  },
   state: {
-
+    api: 'http://localhost:8081/api/v1'
   },
   mutations: {
 
   },
   actions: {
 
+  },
+  getters: {
+    api: state => {
+      return state.api;
+    }
   }
 })
