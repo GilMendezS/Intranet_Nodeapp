656fbde0f1e7d8c7758b6760bac266244e7e1f7d
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2019-02-05 10:44:32 -0600

    :+1: using toast globally

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,9 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Toasted from 'vue-toasted';
 
+Vue.use(Toasted)
 Vue.config.productionTip = false
 
 new Vue({
