c61043bffc5d34be801749cdc1051e109ec1f2e4
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2019-02-07 14:55:56 -0600

    :clipboard: routes added

--- frontend/src/components/includes/Navigation.vue
+++ frontend/src/components/includes/Navigation.vue
@@ -7,73 +7,51 @@
         v-if="userIsAuthenticated"
         >
             <v-list>
-                <v-list-tile>
+                <v-list-tile
+                    router
+                    to="/projects"
+                >
                     <v-list-tile-action>
                         <v-icon>list_alt</v-icon>
                     </v-list-tile-action>
                     <v-list-tile-title>Proyectos</v-list-tile-title>
                 </v-list-tile>
-                <v-list-tile>
+                <v-list-tile
+                    router
+                    to="/viatics"
+                >
                     <v-list-tile-action>
                         <v-icon>attach_money</v-icon>
                     </v-list-tile-action>
                     <v-list-tile-title>Viáticos</v-list-tile-title>
                 </v-list-tile>
-                <v-list-tile>
+                <v-list-tile
+                    router
+                    to='/hours'
+                >
                     <v-list-tile-action>
                         <v-icon>watch_later</v-icon>
                     </v-list-tile-action>
                     <v-list-tile-title>Horas</v-list-tile-title>
                 </v-list-tile>
-                <v-list-group
-                    prepend-icon="account_circle"
-                    value="true"
+                <v-list-tile
+                    router
+                    to='/company'
                 >
-                    <v-list-tile slot="activator">
-                        <v-list-tile-title>Empresa</v-list-tile-title>
-                    </v-list-tile>
-                        
-                    <v-list-group
-                    no-action
-                    sub-group
-                    value="true"
-                    >
-                    <v-list-tile slot="activator">
-                        <v-list-tile-title>Administración de usuarios</v-list-tile-title>
-                    </v-list-tile>
-
-                    <v-list-tile
-                        v-for="(admin, i) in admins"
-                        :key="i"
-                        
-                    >
-                        <v-list-tile-title v-text="admin[0]"></v-list-tile-title>
-                        <v-list-tile-action>
-                        <v-icon v-text="admin[1]"></v-icon>
-                        </v-list-tile-action>
-                    </v-list-tile>
-                    </v-list-group>
-
-                    <v-list-group
-                    sub-group
-                    no-action
-                    >
-                    <v-list-tile slot="activator">
-                        <v-list-tile-title>Administración de áreas</v-list-tile-title>
-                    </v-list-tile>
-
-                    <v-list-tile
-                        v-for="(crud, i) in cruds"
-                        :key="i"
-                        
-                    >
-                        <v-list-tile-title v-text="crud[0]"></v-list-tile-title>
-                        <v-list-tile-action>
-                        <v-icon v-text="crud[1]"></v-icon>
-                        </v-list-tile-action>
-                    </v-list-tile>
-                    </v-list-group>
-                </v-list-group>
+                    <v-list-tile-action>
+                        <v-icon>domain</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-title>Empresa</v-list-tile-title>
+                </v-list-tile>
+                <v-list-tile
+                    @click="onLogout"
+                >
+                    <v-list-tile-action>
+                        <v-icon>power_settings_new</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-title>Salir</v-list-tile-title>
+                </v-list-tile>
+                
             </v-list>
         </v-navigation-drawer>
         <!--toolbar-->
@@ -81,9 +59,7 @@
             <v-toolbar-side-icon v-if="userIsAuthenticated" @click="changeDrawerState"></v-toolbar-side-icon>
             <v-toolbar-title>GMS</v-toolbar-title>
             <v-spacer></v-spacer>
-            <v-toolbar-items class="hidden-sm-and-down">
-                <v-btn v-if="userIsAuthenticated" flat @click="onLogout"> Salir <i class="fa fa-power-off"></i></v-btn>
-            </v-toolbar-items>
+            
         </v-toolbar>
     </div>
     
@@ -98,6 +74,11 @@ import { mapGetters } from 'vuex';
         ],
         cruds: [
             ['Ver estructura', 'business']
+        ],
+        menu: [
+            {title: 'Proyectos', icon: 'list_alt', link: '/projects'},
+            {title: 'Viáticos', icon: 'attach_money', link: '/viatics'},
+            {title: 'Horas', icon: 'watch_later', link: '/hours'},
         ]
     }),
     methods: {
