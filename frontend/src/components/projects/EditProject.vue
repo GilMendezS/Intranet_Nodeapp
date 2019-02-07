e1142913444d69bccd083447d6313c58a767885d
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2019-02-07 12:46:36 -0600

    :clipboard: UI view for edit a project

--- frontend/src/components/projects/EditProject.vue
+++ frontend/src/components/projects/EditProject.vue
@@ -1,17 +1,28 @@
 <template>
-    
+    <v-flex xs12>
+        <v-edit-project-tabs></v-edit-project-tabs>
+    </v-flex>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+import EditProjectTabs from './edit/EditProjectTabs.vue';
 export default {
     props: ['id'],
+    components: {
+        'v-edit-project-tabs':EditProjectTabs
+    },
     mounted(){
         this.getProject();
+        this.$store.dispatch('users/loadUsers');
     },  
     methods: {
         getProject(){
-            
+            this.$store.dispatch('projects/getProject', this.id);
         }
+    },
+    computed:{ 
+        
     }
 }
 </script>
