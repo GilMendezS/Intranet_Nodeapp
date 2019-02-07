09c7f79b55db565e76df404f33b899dbd10fc28f
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2019-02-07 12:48:27 -0600

    :clipboard: new component - table to list all included users in a project

---
+++ frontend/src/components/projects/edit/IncludedUsers.vue
@@ -0,0 +1,80 @@
+<template>
+    <v-card>
+        <v-card-title>
+            <h3>Usuarios en el proyecto</h3>
+            <v-spacer></v-spacer>
+            <v-text-field
+                v-model="search"
+                append-icon="search"
+                label="Search"
+                single-line
+                hide-details
+            ></v-text-field>
+        </v-card-title>
+        <v-card-text>
+            <v-data-table
+                :headers="headers"
+                :items="includedUsers"
+                :search="search"
+                :pagination.sync="pagination"
+                class="elevation-3"
+                >
+                <template slot="items" slot-scope="props">
+                    <td>{{ props.item.name }}</td>
+                    <td class="text-xs-lef">{{ props.item.name }}</td>
+                    <td class="text-xs-left">{{ props.item.lastname }}</td>
+                    <td class="text-xs-left">{{ props.item.email }}</td>
+                    <td class="text-xs-left">{{ props.item.can_add_viatics }}</td>
+                    <td class="text-xs-left">{{ props.item.can_add_hours }}</td>
+                </template>
+                </v-data-table>
+                <div class="text-xs-center pt-2">
+                <v-pagination v-model="pagination.page" :length="pages"></v-pagination>
+                </div>
+        </v-card-text>
+    </v-card>
+</template>
+<script>
+import { mapGetters } from 'vuex';
+export default {
+    data: () => ({
+        search: '',
+        headers: [
+            { text: 'Nombre', align: 'left', value: 'name'},
+            { text: 'Apellidos', align: 'left', value: 'lastname' },
+            { text: 'Email', align: 'left', value: 'email'},
+            { text: 'Regitrar viáticos', value: 'can_add_viatics', sortable: false },
+            { text: 'Regitrar horas', value: 'can_add_hours', sortable: false }
+        ],
+        pagination: {},
+    }),
+    computed: {
+        ...mapGetters({
+            'project': 'projects/getCurrentProject'
+        }),
+        pages () {
+            if (this.pagination.rowsPerPage == null ||
+            this.pagination.totalItems == null
+            ) {
+                return 0
+            }
+            return Math.ceil(this.pagination.totalItems / this.pagination.rowsPerPage)
+        },
+        includedUsers(){
+            return this.project.users.map(u => {
+                return {
+                    id: u.id,
+                    name: u.name,
+                    lastname: u.lastname,
+                    email: u.email,
+                    can_add_viatics: u.project_user.can_add_viatics,
+                    can_add_hours: u.project_user.can_add_hours
+                }
+            })
+            
+            
+        }
+    }
+}
+</script>
+
