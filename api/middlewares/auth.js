b6216ca3ec3aacd97d9a89faf0a8bc390a716bb3
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2019-02-18 11:22:14 -0600

    middleware modified, user instance of sequelize set in request

--- api/middlewares/auth.js
+++ api/middlewares/auth.js
@@ -1,10 +1,12 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/models').User;
 module.exports =  async (req, res, next) => {
     const token = req.headers.authorization;
     if  (token){
         try {
             const user = await jwt.verify(token, process.env.JWT_SECRET)
-            req.user = user;
+            req.user = await User.findByPk(user.id, {include:{all:true}});
+            
             next();
         } catch (error) {
             return res.status(401).json({
