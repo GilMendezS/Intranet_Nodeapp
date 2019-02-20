cce1f75ef2e16400c536ae3f3e8bb5da7c22a708
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2019-02-20 17:34:36 -0600

    :pencil: getters were modified

--- frontend/src/stores/user.js
+++ frontend/src/stores/user.js
@@ -19,7 +19,24 @@ export default {
     },
     getters: {
         getUsers: state => state.users,
-        getActiveUsers: state => state.users.filter( u => u.active),
-        getInativeUsers: state => state.users.filter( u => !u.active )
+        getActiveUsers: state => state.users.filter( u => u.active).map( u => ({
+            id: u.id,
+            name: u.name,
+            lastname: u.lastname,
+            email: u.email,
+            area: u.area ? u.area.title : 'N/A',
+            department: u.department ? u.department.title : 'N/A',
+            position: u.position ? u.position.title : 'N/A',
+
+        })),
+        getInativeUsers:state => state.users.filter( u => !u.active).map( u => ({
+            id: u.id,
+            name: u.name,
+            lastname: u.lastname,
+            email: u.email,
+            area: u.area ? u.area.title : 'N/A',
+            department: u.department ? u.department.title : 'N/A',
+            position: u.position ? u.position.title : 'N/A',
+        }))
     }
 }
