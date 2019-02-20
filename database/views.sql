0f2b0a04ffd2289c82573fb1c01a9cb0ba255719
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2019-02-19 18:12:44 -0600

    :pencil: sql view

--- database/views.sql
+++ database/views.sql
@@ -7,3 +7,15 @@ left join types as t
 on p.type_id = t.id
 left join users as u 
 on u.id = p.user_id;
+
+-- get all data related to viatics
+create or replace view viatics_details as 
+select v.*, 
+s.name as status_name, s.title as status_title, 
+p.code ,
+u.name as user_name, u.lastname as user_lastname, 
+ua.name as authorizator_name, ua.lastname as authorizator_lastname from viatics as v
+inner join statuses as s on s.id = v.status_id
+inner join projects as p on p.id = v.project_id
+inner join users as u on u.id = v.user_id
+left join users as ua on ua.id = auth_user_id;
