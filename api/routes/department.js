e6fd624ac0b195107a841e8f936d7735a6cab72a
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2019-03-29 17:06:46 -0600

    roles middleware applied to the departments resource

--- api/routes/department.js
+++ api/routes/department.js
@@ -1,17 +1,19 @@
 const router = require('express').Router();
 
-const DepartmentController = require('../controllers/department');
+const CheckRoles = require('../middlewares/role');
 
 const Authenticated = require('../middlewares/auth');
 
+const DepartmentController = require('../controllers/department');
+
 router.get('/', Authenticated, DepartmentController.getDepartments);
 
-router.post('/', Authenticated,DepartmentController.addDepartment);
+router.post('/', Authenticated, CheckRoles('admin'), DepartmentController.addDepartment);
 
-router.get('/:id(\\d+)/', Authenticated,DepartmentController.getDepartment);
+router.get('/:id(\\d+)/', Authenticated, DepartmentController.getDepartment);
 
-router.put('/:id(\\d+)/', Authenticated, DepartmentController.updateDepartment);
+router.put('/:id(\\d+)/', Authenticated, CheckRoles('admin'), DepartmentController.updateDepartment);
 
-router.delete('/:id(\\d+)/', Authenticated, DepartmentController.removeDepartment);
+router.delete('/:id(\\d+)/', Authenticated, CheckRoles('admin'), DepartmentController.removeDepartment);
 
 module.exports = router;
